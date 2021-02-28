ba0c7c4b9a866de905ca466d158accc78ba5d02e
Eric Tseng <erictseng89@gmail.com>
2021-02-28 12:40:17 +0800

    pset3 tideman stuck at lock pairs

---
+++ pset3/tideman/recursion.c
@@ -0,0 +1,120 @@
+#include <stdio.h>
+
+// This function will use merge sort to practice recursion.
+// If I want to import this function into tideman:
+
+
+// Prototype
+void rec(int n, int s); 
+
+// Array of numbers.
+int a[10] = {5, 1, 6, 2, 7, 3, 8, 4, 9, 0};
+int a_c = 10;
+// int a[5] = {4, 3, 3, 2, 4};
+// int a_c = 5;
+
+int main(void)
+{
+    int start = 0;
+    rec(10, start);
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("Rank %d has the value %d.\n", i + 1, a[i]);
+    }
+
+}
+
+{
+    // This is the main recursion function. var.int n = how many numbers to sort, var.int s = starting index in the array.
+
+    // If n < 2, meaning theres only 1 or 0 numbers to sort, then return.
+    if (n < 2)
+    {
+        return;
+    }
+
+    // Working out the new variables needed for left and right merge sort.
+    int leftn, rightn, lefts, rights = 0; // variables for left half and right half.
+
+    if (n % 2 == 1) // If odd.
+    {
+        leftn = (n + 1) / 2;
+        rightn = (n - 1 ) / 2;
+        lefts = s;
+        rights = s + (n + 1) / 2;
+    }
+    else // If even.
+    {
+        leftn = n / 2;
+        rightn = n / 2;
+        lefts = s;
+        rights = s + (n / 2);
+    }
+
+    // Merge sort left half and right half.
+    rec(leftn, lefts);
+    rec(rightn, rights);
+
+    // Now to sort the actual left and right arrays.
+    // We need use the leftn and right n value;
+    // as well as new variables here to manage the locations as well as to store the values in the array.
+
+    // current left count and right count.
+    int leftc = 0;
+    int rightc = 0;
+    int b[n]; // temporary array to store the sorted array.
+
+
+    for (int i = 0; i < n ; i++)
+    {
+        int loc = s + i; // location in the new array where we will place the sorted value.
+        int leftloc = lefts + leftc;
+        int rightloc = rights + rightc;
+        int leftv = a[lefts + leftc];
+        int rightv = a[rights + rightc];
+
+        // If there are no more numbers in the left half of the array, the popular new array using right half.
+        if (leftc >= leftn)
+        {
+            b[i] =  rightv;
+            rightc++;
+        }
+        // If there are no more numbers in the right half of the array, the popular new array using left half.
+        else if (rightc >= rightn)
+        {
+            b[i] = leftv;
+            leftc++;
+        }
+        // If left value is greater than right.
+        else if (leftv > rightv)
+        {
+            b[i] = leftv;
+            leftc++;
+        }
+        // If right value is greater than left.
+        else if (rightv > leftv)
+        {
+            b[i] = rightv;
+            rightc++;
+        }
+        else if (leftv == rightv)
+        {
+            b[i] = leftv;
+            leftc++;
+        }
+        else
+        {
+            printf("Sorting error.\n");
+            return;
+        }
+    }
+    // Updating original array with sorted array.
+    for (int i = 0; i < n; i++)
+    {
+        a[s + i] = b[i];
+    }
+}
+
+
+
--- pset3/tideman/tideman.c
+++ pset3/tideman/tideman.c
@@ -1,5 +1,6 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
 // Max number of candidates
 #define MAX 9
@@ -25,11 +26,20 @@ pair pairs[MAX * (MAX - 1) / 2];
 int pair_count;
 int candidate_count;
 
+// Winner loser boolean for lock_pairs and print_winner function.
+bool source[MAX];
+bool loser[MAX];
+
+// Create an array to record all the differences.
+int pair_diff[MAX * (MAX - 1) / 2];
+
 // Function prototypes
 bool vote(int rank, string name, int ranks[]);
 void record_preferences(int ranks[]);
 void add_pairs(void);
 void sort_pairs(void);
+void sort_rec(int n, int s);
+int diff(pair c);
 void lock_pairs(void);
 void print_winner(void);
 
@@ -86,6 +96,19 @@ int main(int argc, string argv[])
 
         record_preferences(ranks);
 
+        /* below is debug printf
+        for (int k = 0; k < voter_count; k++)
+        {
+            printf("#%d voter's preference: ", k);
+            for (int l = 0; l < candidate_count; l++)
+            {
+                printf("%d ", preferences[k][l]);
+            }
+            printf(".\n");
+        }
+
+        */
+
         printf("\n");
     }
 
@@ -100,6 +123,27 @@ int main(int argc, string argv[])
 bool vote(int rank, string name, int ranks[])
 {
     // TODO
+    // This function looks at one voter's preferences.
+    // This function will be given the name of the candidate (var.string name).
+    // It will be given the voter's rank for this candidate (var.int rank).
+    // It will then update the var.array-int ranks[].
+    // Use strcmp to make sure the name of the candidate is correct.
+
+    // Search for candidate name.
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // If name is found, then update ranks.
+        if (strcmp(name, candidates[i]) == 0)
+        {
+            /* Below printf is for debugging */
+            // printf("strcmp result: %s and candidate #%d who is %s returned 0.\n", name, i + 1, candidates[i]);
+
+            ranks[rank] = i;
+            return true;
+        }
+    }
+
+    // If name is not found, return false.
     return false;
 }
 
@@ -107,6 +151,42 @@ bool vote(int rank, string name, int ranks[])
 void record_preferences(int ranks[])
 {
     // TODO
+    // This function is called once for each voter, after the array int ranks[] has been tallied.
+    // This function will update the array.preferences[i][j].
+    // ranks[0] will be preferred over every other candidate.
+    // 1. preferences[ranks[0]][j] will add by one for each j.
+    // ?Create a bool array with candidates to identify whether they have been tallied yet.
+    // Something like tallied[candidate_count]. We will go through each preferences[rank[]][j]
+    // If tallied[j] is true, then skip this vote, otherwise add by one.
+
+    // tallied will record whether the ith where i is the same as preferences.
+    bool tallied[candidate_count];
+    for (int k = 0; k < candidate_count; k++)
+    {
+        tallied[k] = false;
+    }
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        // printf("ranks[%d] value is %d.\n", i, ranks[i]);
+
+        for (int j = 0; j < candidate_count; j++)
+        {
+            // printf("tallied[%d] value is %d. Where 1 is true, and 0 if false.\n", j, tallied[j]);
+            if (ranks[i] != j)
+            {
+
+                if (!tallied[j])
+                {
+                    preferences[ranks[i]][j]++;
+                    // printf("Current preference[%d][%d] is: %d.\n", ranks[i], j, preferences[ranks[i]][j]);
+                }
+            }
+
+        }
+        tallied[ranks[i]] = true;
+    }
+
     return;
 }
 
@@ -114,6 +194,28 @@ void record_preferences(int ranks[])
 void add_pairs(void)
 {
     // TODO
+    // Need to use pair_count to record which pair[k] to update.
+    // For each preferences[i][j] pair, see if preferences[i][j] is greater than preferences[j][i].
+    // If so, then add the values in the array.pairs[k].
+    //
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        for (int j = 0; j < candidate_count; j++)
+        {
+            // printf("Checking preferences[%d][%d] to see if there is a pair.\n", i, j);
+            if (preferences[i][j] > preferences[j][i])
+            {
+                // printf("The above preference is a pair. %d > %d.\n", preferences[i][j], preferences[j][i]);
+                pairs[pair_count].winner = i;
+                pairs[pair_count].loser = j;
+                // printf("pairs[%d]'s winner and losers are: #%d and #%d.\n", pair_count, i, j);
+                pair_count++;
+                // printf("pair count is current at: %d.\n", pair_count);
+
+            }
+        }
+    }
     return;
 }
 
@@ -121,6 +223,107 @@ void add_pairs(void)
 void sort_pairs(void)
 {
     // TODO
+    // Create a sorting function using recursion.
+    // Note that each pair actually represents a number. So think of a recursive number sorter.
+    // Use the int pair_count. Sort left half of pair_count, then sort right half.
+    // After sort, then arrange them. compare two values, the higher goes next into pairs variable.
+    // Do I need to create a copy of pairs?
+
+    sort_rec(pair_count, 0);
+    return;
+}
+
+int diff(pair c)
+{
+    int i = c.winner;
+    int j = c.loser;
+    return (preferences[i][j] - preferences[j][i]);
+}
+
+void sort_rec(int n, int s)
+{
+    // This is the main recursion function. var.int n = how many numbers to sort, var.int s = starting index in the array.
+
+    // If n < 2, meaning theres only 1 or 0 numbers to sort, then return.
+    if (n < 2)
+    {
+        return;
+    }
+
+    // Working out the new variables needed for left and right merge sort.
+    int leftn, rightn, lefts, rights = 0; // variables for left half and right half.
+
+    if (n % 2 == 1) // If odd.
+    {
+        leftn = (n + 1) / 2;
+        rightn = (n - 1 ) / 2;
+        lefts = s;
+        rights = s + (n + 1) / 2;
+    }
+    else // If even.
+    {
+        leftn = n / 2;
+        rightn = n / 2;
+        lefts = s;
+        rights = s + (n / 2);
+    }
+
+    // Merge sort left half and right half.
+    sort_rec(leftn, lefts);
+    sort_rec(rightn, rights);
+
+    // current left count and right count.
+    int leftc = 0;
+    int rightc = 0;
+    pair temp[n]; // temporary array to store the sorted array.
+
+
+    for (int i = 0; i < n ; i++)
+    {
+        // These are probably not necessary.
+        pair leftv = pairs[lefts + leftc];
+        pair rightv = pairs[rights + rightc];
+
+        // If there are no more numbers in the left half of the array, then populate new array using right half.
+        if (leftc >= leftn)
+        {
+            temp[i] =  rightv;
+            rightc++;
+        }
+        // If there are no more numbers in the right half of the array, the popular new array using left half.
+        else if (rightc >= rightn)
+        {
+            temp[i] = leftv;
+            leftc++;
+        }
+        // If left value is greater than right.
+        else if (diff(leftv) > diff(rightv))
+        {
+            temp[i] = leftv;
+            leftc++;
+        }
+        // If right value is greater than left.
+        else if (diff(rightv) > diff(leftv))
+        {
+            temp[i] = rightv;
+            rightc++;
+        }
+        else if (diff(leftv) == diff(rightv))
+        {
+            temp[i] = leftv;
+            leftc++;
+        }
+        else
+        {
+            // printf("Sorting error.\n");
+            // return;
+        }
+    }
+    // Updating original array with sorted array.
+    for (int i = 0; i < n; i++)
+    {
+        pairs[s + i] = temp[i];
+    }
     return;
 }
 
@@ -128,6 +331,52 @@ void sort_pairs(void)
 void lock_pairs(void)
 {
     // TODO
+
+    // To prevent a cycle, we will use the following system:
+    // If a candidate has already a been loser in a previous pair, then the candidate can no longer point to a current source.
+    // Thus there needs to be an int variable to see who is the current source and a boolean array to see who has been a loser.
+
+    // First source will be the winner in the first pair.
+
+    for (int i = 0; i <candidate_count; i++)
+    {
+        source[i] = false;
+        loser[i] = false;
+    }
+
+
+    // Then go through each pair.
+    for (int i = 0; i < pair_count; i++)
+    {
+        // Create a temporary var.pair to ease coding readability. c for current.
+        pair c = pairs[i];
+
+        if (loser[c.winner])
+        {
+            if (source[c.loser])
+            {
+                // If a loser has a win over a source, then do nothing.
+                ;
+            }
+            else
+            {
+                // Or else just record the locked pair.
+                locked[c.winner][c.loser] = true;
+                loser[c.loser] = true;
+            }
+        }
+        else if (!loser[c.winner])
+        {
+            // If a winner has never lost before, then they become a source. If their opponent was once a source, they are no longer a source.
+            locked[c.winner][c.loser] = true;
+            source[c.winner] = true;
+            loser[c.loser] = true;
+            if (source[c.loser])
+            {
+                source[c.loser] = false;
+            }
+        }
+    }
     return;
 }
 
@@ -135,6 +384,17 @@ void lock_pairs(void)
 void print_winner(void)
 {
     // TODO
+    // Parse through locked array. locked[i][j]
+    // See for which candidate, where when they are the j, the result is all false.
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (source[i])
+        {
+            printf("%s", candidates[source[i]]);
+        }
+    }
     return;
 }
+
+
 
